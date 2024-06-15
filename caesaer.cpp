6b970694f82840077e4d44423f3a8125f7e4a6a4
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2024-06-15 15:51:15 +0300

    encrypt and decrypt was added. dll was created

--- caesaer.cpp
+++ caesaer.cpp
@@ -1,15 +1,60 @@
-#include <iostream>
 #include <string>
+#include <cctype> //for upper and lower case
 
 using namespace std;
 
 extern "C" {
-__declspec(dllexport)
-void encrypt (string text, int key) {
+__declspec(dllexport) string encrypt(string text, int key);
+__declspec(dllexport) string decrypt(string text, int key);
 
-}
+    string encrypt (string text, int key) {
+        string cipher = "";
 
-void decrypt (string text, int key) {
-    
-}
+        for (unsigned i=0; i < text.length(); i++ ) {
+            char c = text[i];
+
+            if (isupper(c)) {
+                //apply Ceaser Cipher formula
+                int x = c - 'A' + key;
+                //handle boundary condition
+                x = x % 26;
+                //convert to char
+                c = (char)(x + 'A');
+            }
+            else if (islower(c)) {
+                int x = c - 'a' + key;
+                x = x % 26;
+                c = (char)(x + 'a');
+            }
+            cipher.insert(cipher.end(), 1, c); //insert in the end
+        }
+
+        return cipher;
+    }
+
+    string decrypt (string text, int key) {
+        string plain = "";
+
+        for (unsigned i=0; i < text.length(); i++ ) {
+            char c = text[i];
+
+            if (isupper(c)) {
+                int x = c - 'A' - key;
+                if (x < 0) {
+                    x += 26;
+                }
+                c = (char)(x + 'A');
+            }
+            else if (islower(c)) {
+                int x = c - 'a' - key;
+                if (x < 0) {
+                    x += 26;
+                }
+                c = (char)(x + 'a');
+            }
+            plain.insert(plain.end(), 1, c);
+        }
+
+        return plain;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    HINSTANCE handle = LoadLibrary(TEXT("C:\KSE\Programming_paradigms\Assignment_3\caesaer.dll"));
+    HINSTANCE handle = LoadLibrary(TEXT("C:\\KSE\\Programming_paradigms\\Assignment_3\\caesaer.dll"));
 
     if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
     {
@@ -12,20 +12,60 @@ int main() {
         return -1;
     }
 
-    typedef void(*function_ptr)(string, string, int);
-    function_ptr encrypt = (function_ptr)GetProcAddress(handle, "encrypt");
+    typedef string(*encrypt_ptr)(string, int);
+    auto encrypt = (encrypt_ptr)GetProcAddress(handle, "encrypt"); //encrypt_ptr -> auto = to avoid duplicating the type name
     if (encrypt == nullptr)
     {
         cout << "Proc not found" << endl;
         return -1;
     }
 
-    typedef void(*function_ptr)(string, string, int);
-    function_ptr decrypt = (function_ptr)GetProcAddress(handle, "encrypt");
+    typedef string(*decrypt_ptr)(string, int);
+    auto decrypt = (decrypt_ptr)GetProcAddress(handle, "decrypt");
     if (decrypt == nullptr)
     {
         cout << "Proc not found" << endl;
         return -1;
+    }
+
+    while(true) {
+        cout << "Choose command (cipher/exit):";
+        string command;
+        cin >> command;
+
+        if (command == "exit") {
+            cout << "Program was canselled" << endl;
+            break;
+        }
+        else if (command == "cipher") {
+            cout << "Please enter the key:";
+            int key;
+            cin >> key;
+
+            string text = "";
+            cout << "Please enter the text:" << endl;
+            cin >> text;
+            string cipher = "";
+
+            cout << endl;
+
+            cipher = encrypt(text, key);
+            cout << "ENCRYPTING" << endl;
+            cout << "Plain text:" << endl;
+            cout << text << endl;
+            cout << "Cipher text:" << endl;
+            cout << cipher << endl << endl;
+
+            text = decrypt(cipher, key);
+            cout << "DECRYPTING" << endl;
+            cout << "Cipher text:" << endl;
+            cout << cipher << endl;
+            cout << "Plain text:" << endl;
+            cout << text << endl << endl;
+        }
+        else {
+            cout << "Invalid command" << endl;
+        }
     }
 
     FreeLibrary(handle);
